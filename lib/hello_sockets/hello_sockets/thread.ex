0e22e956eb9147e5caedee25a488b921f143c886
Berci Kormendy <korber314@gmail.com>
2023-01-02 23:34:47 +0100

    derive jason.encoder for Thread

--- lib/hello_sockets/hello_sockets/thread.ex
+++ lib/hello_sockets/hello_sockets/thread.ex
@@ -2,6 +2,7 @@ defmodule HelloSockets.Thread do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:id, :created_at, :updated_at, :author_id, :workspace_id, :deleted, :resolved, :canvas_coords]}
   @primary_key false
   schema "threads" do
     field :id, :string, primary_key: true
